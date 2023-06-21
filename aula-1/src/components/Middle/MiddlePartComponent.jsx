780d7a332c70d8fbb7c27df9782967c6e4fc43e7
thales santos <thalesabsantos@gmail.com>
2023-06-20 23:27:16 -0300

    .

--- aula-1/src/components/Middle/MiddlePartComponent.jsx
+++ aula-1/src/components/Middle/MiddlePartComponent.jsx
@@ -3,7 +3,8 @@ import './MiddlePartComponent.css'
 const MiddlePartComponent = () => {
   return (
     <div className='container'>
-        <h1>MiddlePartComponent</h1>
+        <h1 className='titulo'>Curta o Premium grátis por 2 meses</h1>
+        <h3>Depois, pague somente R$ 19,90/mês. Cancele quando quiser.</h3>
     </div>
   )
 }
