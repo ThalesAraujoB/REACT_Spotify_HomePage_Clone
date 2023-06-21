c83a1e286422522b695231f12cad56fea53d242c
thales santos <thalesabsantos@gmail.com>
2023-06-20 23:18:38 -0300

    segunda-parte

--- aula-1/src/App.jsx
+++ aula-1/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NavBarComponent from './components/navbar/NavBarComponent'
+import MiddlePartComponent from "./components/Middle/MiddlePartComponent";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <NavBarComponent></NavBarComponent>
+      <MiddlePartComponent></MiddlePartComponent>
     </div>
   )
 }
---
+++ aula-1/src/components/Middle/MiddlePartComponent.jsx
@@ -0,0 +1,11 @@
+import './MiddlePartComponent.css'
+
+const MiddlePartComponent = () => {
+  return (
+    <div className='container'>
+        <h1>MiddlePartComponent</h1>
+    </div>
+  )
+}
+
+export default MiddlePartComponent
