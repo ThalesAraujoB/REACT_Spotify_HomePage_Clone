c02b7d6f2ba304a85761a3afd1abb4ba5c57ec77
thales santos <thalesabsantos@gmail.com>
2023-06-20 23:42:01 -0300

    segunda parte completa

--- aula-1/src/components/Middle/MiddlePartComponent.jsx
+++ aula-1/src/components/Middle/MiddlePartComponent.jsx
@@ -5,6 +5,9 @@ const MiddlePartComponent = () => {
     <div className='container'>
         <h1 className='titulo'>Curta o Premium grátis por 2 meses</h1>
         <h3>Depois, pague somente R$ 19,90/mês. Cancele quando quiser.</h3>
+        <button className='btn1'>COMECE AGORA</button>
+        <button className='btn2'>VER PLANOS</button>
+        <p className='termos'><u>Sujeito a Termos e Condições</u>. Os 2 meses grátis não estão disponíveis para usuários que já experimentaram o Premium.</p>
     </div>
   )
 }
