55600eae46cc7ba0e9e7114e04897466eb3febc5
thales santos <thalesabsantos@gmail.com>
2023-06-20 22:40:37 -0300

    primeira-parte

---
+++ aula-1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBarComponent from './components/navbar/NavBarComponent'
+
+
+function App() {
+
+  return (
+    <div>
+      <NavBarComponent></NavBarComponent>
+    </div>
+  )
+}
+
+export default App
---
+++ aula-1/src/components/navbar/NavBarComponent.jsx
@@ -0,0 +1,18 @@
+import './NavBarComponent.css'
+const NavBarComponent = () => {
+  return (
+    <div className='navbar'>
+      <img src="./data/logobg.png" alt="" />
+      <div className='tags'>
+      <a href="">Premium</a>
+      <a href="">Suporte</a>
+      <a href="">Baixar</a>
+      <p>|</p>
+      <a href="">Inscrever-se</a>
+      <a href="">Entrar</a>
+      </div>
+    </div>
+  )
+}
+
+export default NavBarComponent
