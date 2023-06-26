0b36ffb0248fda9c920f0f07d97589cad12d66f2
thales santos <thalesabsantos@gmail.com>
2023-06-26 14:40:24 -0300

    dinamizando elementos filhos

--- aula-1/src/components/premium/PremiumComponent.jsx
+++ aula-1/src/components/premium/PremiumComponent.jsx
@@ -2,10 +2,21 @@ import './PremiumComponent.css'
 import FeaturesComponent from './features/FeaturesComponent'
 
 const PremiumComponent = () => {
+
+
   return (
-    <div className='container-box'>
+    <div className='big-box'>
+      <div className='container-box'>
       <h1 className='title'>Por que ser Premium?</h1>
-      <FeaturesComponent></FeaturesComponent>
+      </div>
+    <div className='features-box'>
+    <FeaturesComponent 
+    imagebase='/data/music-bg.png'
+    titulobox='exemplo'
+    mensagembox='exemplos dinamico'
+
+    />
+      </div>
     </div>
   )
 }
--- aula-1/src/components/premium/features/FeaturesComponent.jsx
+++ aula-1/src/components/premium/features/FeaturesComponent.jsx
@@ -1,9 +1,29 @@
 import './FeaturesComponent.css'
 
-const FeaturesComponent = () => {
+const FeaturesComponent = ({imagembase,titulobox,mensagembox}) => {
   return (
-    <div>FeaturesComponent</div>
+    <div className='box-featurs'>
+      <img src={imagembase} className='imagem-base'/>
+      <h2 className='titulo-box'>{titulobox}</h2>
+      <p className='subtitle-box'>{mensagembox}</p>
+    </div>
   )
 }
 
+
 export default FeaturesComponent
+
+
+/*
+
+const FeaturesComponent = ({ text, image }) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+      <img src={image} alt="Imagem dinâmica" />
+    </div>
+  );
+};
+
+export default FeaturesComponent;
+ */
